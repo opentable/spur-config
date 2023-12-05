9147dda78568c587be9df016fcda2d24a44d50ec
Agustin Colchado <agustin@colchado.com>
2023-12-05 01:13:33 -0800

    Migrates testing to use Jest and normalizes the function scopes
    Mocha, chai and sinon are slowly losing support and start conflicting with dependencies. Moving to Jest to future proof and make it a common use within our group.

--- test/globals.js
+++
@@ -1,8 +0,0 @@
-global.assert = require('assert');
-global.chai = require('chai');
-global.sinon = require('sinon');
-
-global.expect = global.chai.expect;
-
-process.env.NODE_ENV = 'test';
-process.setMaxListeners(1000);
